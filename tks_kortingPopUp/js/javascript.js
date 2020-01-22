3300c5442a211ab3bfa18ece945376e30842ddce
dylan <dylanspin100@hotmail.com>
2020-01-22 15:25:36 +0100

    Begin

---
+++ tks_kortingPopUp/js/javascript.js
@@ -0,0 +1,5 @@
+
+	function closekortingblock(){
+		document.getElementById('popUpKorting').style.display = "none";
+		document.cookie = "popUp2=true;";
+	}
---
+++ tks_kortingPopUp/tks_kortingPopUp.php
@@ -0,0 +1,73 @@
+
+<?php
+
+
+  //@author     Dylan spin
+
+    defined('_JEXEC') or die;
+    jimport('joomla.plugin.plugin');
+
+    class PlgSystemtks_popUpKorting extends JPlugin{
+
+        protected $app;
+        public $params;
+
+        function __construct(&$subject, $config) {
+            parent :: __construct($subject, $config);
+        }
+
+        public function createCon($pluginParams){
+
+            $korting = $pluginParams->get('korting');
+            $popuptext = $pluginParams->get('popuptext');
+
+            $cont = "<div class='popUpOverlay_Korting' id='popUpKorting' onclick='closekortingblock()'>
+                        <div class='popUp_Korting'>
+                            <div class='innerPopup_Korting'>
+                              <div class='popUpKop_Korting'>50% korting</div>
+                            </div>
+                        </div>
+                    </div>
+                    ";
+
+      	    return $cont;
+
+      	}
+
+        public function onAfterDispatch(){
+
+            $plugin = JPluginHelper::getPlugin('system', 'tks_popUp');
+            $pluginParams = new JRegistry($plugin->params);
+
+            $plgURL = JURI::base() . 'plugins/system/tks_popUp';
+            $doc = JFactory::getDocument();
+
+            $doc->addStyleSheet($plgURL . '/css/style.css');
+            if(!isset($_COOKIE['popUp'])){
+                $doc->addScript($plgURL.'/js/advertisement.js');//bait script voor de adblocker
+                $doc->addScript($plgURL.'/js/javascript.js');
+            }
+
+            $this->params = $pluginParams;
+
+        }
+
+        public function onAfterRender(){
+
+            $getapp = JFactory::getApplication();
+            $pluginParams = $this->params;
+            $body = $this->app->getBody();
+            $content = $this->createCon($pluginParams);
+            $body = str_replace('</body>', $content . '</body>', $body );
+
+            if($this->app->isSite()){
+                if(!isset($_COOKIE['popUp'])){
+                    $this->app->setBody($body);
+                }
+            }
+
+        }
+
+    }
+
+?>
